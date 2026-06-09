89f7a74175bac8b2cc00c8b41ceb6a855ff57681
Santosh S <santos@akamai.com>
2026-06-09 13:10:33 +0000

    Pull request #373: upcoming: [DPS-42588] - Handling unauthorized errors for Entities component
    Merge in FEE/cloud-manager from feature/DPS-42588-entities-table-unauthorized-error-handling to develop
    
    Squashed commit of the following:
    
    commit 47b9ea0cae81c9edda5d80977c5a51dca53ea352
    Author: santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
    Date:   Mon Jun 8 23:19:30 2026 +0530
    
        add changeset
    
    commit 93a20b10dbedb5827142a5ba6ecd73d2e2c4e896
    Author: santoshp210-akamai <159890961+santoshp210-akamai@users.noreply.github.com>
    Date:   Fri May 29 12:36:25 2026 +0530
    
        upcoming: [DPS-42588] - Handling unauthorized errors for Entities component

---
+++ packages/manager/.changeset/pr-373-upcoming-features-1780940947470.md
@@ -0,0 +1,5 @@
+---
+"@linode/manager": Upcoming Features
+---
+
+Handle Unauthorized error message state for cloudpulse alerts Entities component ([#373](https://git.source.akamai.com/projects/FEE/repos/cloud-manager/pull-requests/373))
--- packages/manager/src/features/CloudPulse/Alerts/AlertsResources/AlertsResources.tsx
+++ packages/manager/src/features/CloudPulse/Alerts/AlertsResources/AlertsResources.tsx
@@ -1,5 +1,11 @@
 import { useRegionsQuery } from '@linode/queries';
-import { Checkbox, CircleProgress, Stack, Typography } from '@linode/ui';
+import {
+  Checkbox,
+  CircleProgress,
+  Notice,
+  Stack,
+  Typography,
+} from '@linode/ui';
 import { GridLegacy, useTheme } from '@mui/material';
 import React from 'react';
 
@@ -219,6 +225,7 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
     data: resources,
     isError: isResourcesError,
     isLoading: isResourcesLoading,
+    error,
   } = useResourcesQuery(
     Boolean(
       serviceType &&
@@ -432,6 +439,13 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
       ? Math.max(0, maxSelectionCount - selectedResources.length)
       : undefined;
 
+  const isUnAuthorizedError =
+    isResourcesError &&
+    ((error instanceof Error && error.message === 'Unauthorized') ||
+      (error instanceof Array &&
+        error.length > 0 &&
+        error[0]?.reason === 'Unauthorized'));
+
   return (
     <Stack gap={2}>
       {isLoading && (
@@ -539,6 +553,14 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
             />
           </GridLegacy>
         )}
+        {isUnAuthorizedError && (
+          <GridLegacy item xs={12}>
+            <Notice
+              text="You don't have permission to view the entities behind these metrics. Contact your account administrator to request access."
+              variant="warning"
+            />
+          </GridLegacy>
+        )}
         {maxSelectionCount !== undefined && (
           <GridLegacy item xs={12}>
             <AlertListNoticeMessages
@@ -553,6 +575,7 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
           </GridLegacy>
         )}
         {isSelectionsNeeded &&
+          !isUnAuthorizedError &&
           !isDataLoadingError &&
           regionFilteredResources &&
           regionFilteredResources.length > 0 && (
@@ -570,7 +593,7 @@ export const AlertResources = React.memo((props: AlertResourcesProp) => {
           <DisplayAlertResources
             filteredResources={filteredResources}
             handleSelection={handleSelection}
-            isDataLoadingError={isDataLoadingError}
+            isDataLoadingError={!isUnAuthorizedError && isDataLoadingError}
             isSelectionsNeeded={isSelectionsNeeded}
             maxSelectionCount={maxSelectionCount}
             scrollToElement={() =>
