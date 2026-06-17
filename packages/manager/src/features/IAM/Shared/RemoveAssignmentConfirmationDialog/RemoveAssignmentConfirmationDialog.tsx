052ef99739b904c3fb34c700721e7c827bb0a783
Alban Bailly <abailly@akamai.com>
2026-06-17 07:47:59 +0000

    Pull request #423: [UIE-11698] - Break entity name in delete modal to prevent overflow
    Merge in FEE/cloud-manager from UIE-11698 to develop
    
    Squashed commit of the following:
    
    commit e549f69c6bed26830095745cf58df9c14bad7e2e
    Author: Alban Bailly <abailly@akamai.com>
    Date:   Tue Jun 16 16:44:19 2026 +0200
    
        break entity name to prevent overflow

--- packages/manager/src/features/IAM/Shared/RemoveAssignmentConfirmationDialog/RemoveAssignmentConfirmationDialog.tsx
+++ packages/manager/src/features/IAM/Shared/RemoveAssignmentConfirmationDialog/RemoveAssignmentConfirmationDialog.tsx
@@ -123,12 +123,18 @@ export const RemoveAssignmentConfirmationDialog = (props: Props) => {
           {isDefaultDelegationRolesForChildAccount ? (
             <Typography>
               Delegate users won’t get the <strong>{role?.role_name}</strong>{' '}
-              access on the <strong>{role?.entity_name}</strong> entity by
-              default.
+              access on the{' '}
+              <strong style={{ wordBreak: 'break-word' }}>
+                {role?.entity_name}
+              </strong>{' '}
+              entity by default.
             </Typography>
           ) : (
             <Typography>
-              You’re about to remove the <strong>{role?.entity_name}</strong>{' '}
+              You’re about to remove the{' '}
+              <strong style={{ wordBreak: 'break-word' }}>
+                {role?.entity_name}
+              </strong>{' '}
               entity from the <strong>{role?.role_name}</strong> role for{' '}
               <strong>{username}</strong>. This change will be applied
               immediately.
