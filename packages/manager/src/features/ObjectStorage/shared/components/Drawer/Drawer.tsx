e533c931bee3716108ee8c7bde02ac1c89e409bc
Sebastian Kulpok <skulpok@akamai.com>
2026-08-07 06:53:22 +0000

    Pull request #665: STORIF-553: Corrected a bug where onClose event was called on Drawer initialization.
    Merge in FEE/cloud-manager from bugfix/STORIF-553-bucket-tab-is-opened-after-navigating-to-object-storage to develop
    
    Squashed commit of the following:
    
    commit d05bcee3774fc0298a551fa40e55a14026238bfe
    Author: Sebastian Kulpok <skulpok@akamai.com>
    Date:   Thu Aug 6 15:35:47 2026 +0200
    
        STORIF-533: removed special handling of BeforeClose event
    
    commit b8ae4a35d331368bea24be6b427e72864015cd9a
    Author: Sebastian Kulpok <skulpok@akamai.com>
    Date:   Thu Aug 6 15:30:52 2026 +0200
    
        Added changeset: Correct a bug where onClose/onBeforeClose event is called on Drawer initialization
    
    commit 70f08892e063e01efccdc0eb656db89cf7eda713
    Author: Sebastian Kulpok <skulpok@akamai.com>
    Date:   Thu Aug 6 15:26:09 2026 +0200
    
        STORIF-553: Corrected a bug where onClose event was called on Drawer initialization.

--- packages/manager/src/features/ObjectStorage/shared/components/Drawer/Drawer.tsx
+++ packages/manager/src/features/ObjectStorage/shared/components/Drawer/Drawer.tsx
@@ -1,8 +1,33 @@
 import { Drawer as CDSDrawer } from '@akamai/cds-components/react';
 import type { ComponentProps } from 'react';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 export function Drawer(props: ComponentProps<typeof CDSDrawer>) {
-  return createPortal(<CDSDrawer {...props} />, document.body);
+  const { onClose, open, ...rest } = props;
+  const wasOpenRef = useRef(false);
+
+  if (open) {
+    wasOpenRef.current = true;
+  }
+
+  // Workaround for CDSDrawer bug that calls onClose during initialization
+  // Only propagate events if the drawer was previously opened
+  const handleClose = useCallback(
+    (event: CustomEvent<unknown>) => {
+      if (wasOpenRef.current && onClose) {
+        onClose(event);
+      }
+    },
+    [onClose]
+  );
+
+  return createPortal(
+    <CDSDrawer
+      {...rest}
+      onClose={handleClose}
+      open={open}
+    />,
+    document.body
+  );
 }
